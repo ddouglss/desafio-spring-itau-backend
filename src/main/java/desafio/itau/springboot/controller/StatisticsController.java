0f04453428caeef1da1879a753cf9219912572fa
ddouglss <ddouglss1999@gmail.com>
2025-06-22 00:38:15 -0300

    feat: Controller de estatistica criado

---
+++ src/main/java/desafio/itau/springboot/controller/StatisticsController.java
@@ -0,0 +1,10 @@
+package desafio.itau.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/estatistica")
+public class StatisticsController {
+
+}
